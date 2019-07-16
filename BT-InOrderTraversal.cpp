d7f289a88d4c635f8c5894ae29321d0bbbbe409e
eric1162004 <49351150+eric1162004@users.noreply.github.com>
2019-07-16 12:54:09 -0700

    Add files via upload

---
+++ BT-InOrderTraversal.cpp
@@ -0,0 +1,64 @@
+//Binary tree - Depth first In-order Traversal
+
+#include <iostream>
+using namespace std;
+
+struct BstNode
+{
+    int data;
+    BstNode *left;
+    BstNode *right;
+};
+
+BstNode *GetNewNode(int data)
+{
+    BstNode *newNode = new BstNode();
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+BstNode *Insert(BstNode *root, int data)
+{
+    if (root == NULL)
+    {
+        root = GetNewNode(data);
+        return root;
+    }
+
+    else if (data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+void InOrder(BstNode *root)
+{
+    if (root == NULL)
+        return;
+    InOrder(root->left);
+    printf("%d ", root->data);
+    InOrder(root->right);
+}
+
+int main()
+{
+    BstNode *rootPtr = NULL;
+
+    rootPtr = Insert(rootPtr, 8);
+    rootPtr = Insert(rootPtr, 2);
+    rootPtr = Insert(rootPtr, 1);
+    rootPtr = Insert(rootPtr, 3);
+    rootPtr = Insert(rootPtr, 10);
+    rootPtr = Insert(rootPtr, 9);
+    rootPtr = Insert(rootPtr, 11);
+
+    InOrder(rootPtr);
+}
